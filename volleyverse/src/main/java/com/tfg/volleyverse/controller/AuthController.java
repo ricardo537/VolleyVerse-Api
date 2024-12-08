6760ece986d2946725aaa2b70a8ec7fd885eea42
ricardo537 <dawricardo735@gmail.com>
2024-12-08 01:16:56 +0100

    Login + delete funcionales

--- volleyverse/src/main/java/com/tfg/volleyverse/controller/AuthController.java
+++ volleyverse/src/main/java/com/tfg/volleyverse/controller/AuthController.java
@@ -8,56 +8,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tfg.volleyverse.dto.ClubDTO;
 import com.tfg.volleyverse.dto.LoginDTO;
-import com.tfg.volleyverse.dto.RegisterClubDTO;
-import com.tfg.volleyverse.service.imp.ClubServiceImp;
-import com.tfg.volleyverse.service.imp.PlayerServiceImp;
+import com.tfg.volleyverse.service.imp.UserServiceImp;
 
 @RestController
 @RequestMapping("volleyverse/api/v1/auth")
 public class AuthController {
 	
 	@Autowired
-	private ClubServiceImp clubService;
+	private UserServiceImp userService;
 	
-	@Autowired
-	private PlayerServiceImp userService;
-	
-	//Falta cambiarlo para que solo se pueda guardar un email por perfil sea de usuario o de club
 	@PostMapping("/login")
 	public ResponseEntity<LoginDTO> login (@RequestBody LoginDTO login) {
-		return new ResponseEntity<LoginDTO>(login, HttpStatus.OK);
+		LoginDTO success = this.userService.login(login);
+		if (success != null) {
+			return new ResponseEntity<LoginDTO>(success, HttpStatus.OK);
+		} 
+		return new ResponseEntity<LoginDTO>(success, HttpStatus.NOT_FOUND);
 	}
 	
-	//Falta cambiarlo para que filtre según el tipo
-	/*
 	@PostMapping("/delete")
 	public ResponseEntity<Boolean> delete (@RequestBody LoginDTO login) {
-		LoginDTO success = this.clubService.loginClub(login);
-		Boolean response = false;
-		if (success != null) {
-			response = this.clubService.deleteClub(login);
-			if (response) {
-				return new ResponseEntity<>(response, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-			}
-		} else {
-			success = this.userService.loginUser(login);
-			if (success != null) {
-				response = this.userService.deleteUser(login);
-				if (response) {
-					return new ResponseEntity<>(response, HttpStatus.OK);
-				} else {
-					return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-				}
-			} else {
-				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);	
-			}
+		boolean success = this.userService.delete(login);
+		if (success) {
+			return new ResponseEntity<Boolean>(success, HttpStatus.OK);
 		}
+		return new ResponseEntity<Boolean>(success, HttpStatus.NOT_FOUND);
 	}
 	
+	/*
 	@PostMapping("/getProfile")
 	public ResponseEntity<Object> getProfile (@RequestBody LoginDTO login) {
 		Object success = null;
--- volleyverse/src/main/java/com/tfg/volleyverse/service/imp/UserServiceImp.java
+++ volleyverse/src/main/java/com/tfg/volleyverse/service/imp/UserServiceImp.java
@@ -1,10 +1,16 @@
 package com.tfg.volleyverse.service.imp;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tfg.volleyverse.dto.LoginDTO;
+import com.tfg.volleyverse.model.Club;
+import com.tfg.volleyverse.model.Player;
 import com.tfg.volleyverse.model.User;
+import com.tfg.volleyverse.repository.ClubRepository;
+import com.tfg.volleyverse.repository.PlayerRepository;
 import com.tfg.volleyverse.repository.UserRepository;
 import com.tfg.volleyverse.service.UserService;
 
@@ -13,19 +19,46 @@ public class UserServiceImp implements UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PlayerRepository playerRepository;
+	@Autowired
+	private ClubRepository clubRepository;
 
 	@Override
 	public LoginDTO login(LoginDTO login) {
 		User user = this.userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword());
 		if (user != null) {
-			return login;
+			return new LoginDTO(user);
 		}
 		return null;
 	}
 
 	@Override
 	public boolean delete(LoginDTO login) {
-		// TODO Auto-generated method stub
+		User user = this.userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword());
+		if (user != null) {
+			//Cuando pongo otro case "player" no funciona hay que revisarlo
+			switch(login.getType()) {
+			case ("club") : {
+				Optional<Club> club = this.clubRepository.findById(user.getId_user());
+				if (club.isEmpty()) {
+					return false;
+				}
+				this.clubRepository.delete(club.get());
+				break;
+			}
+			default : {
+				Optional<Player> player = this.playerRepository.findById(user.getId_user());
+				if (player.isEmpty()) {
+					return false;
+				}
+				this.playerRepository.delete(player.get());
+				break;
+			}
+			}
+			this.userRepository.delete(user);
+			return true;
+		}
 		return false;
 	}
 
